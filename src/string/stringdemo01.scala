1ec900777de05d562199e26fab515df111574223
yangshenghua <2103025644@qq.com>
2020-05-03 02:12:49 +0800

    Allcommit

--- src/string/stringdemo01.scala
+++ src/string/stringdemo01.scala
@@ -2,7 +2,12 @@ package string
 
 object stringdemo01 {
   var name : String = "yangshegnhua"
+  var sbu = new StringBuilder;
+  sbu++="yang"
+  sbu ++= "a"
+  sbu ++= "yang"
   def main(args: Array[String]): Unit = {
     println(name)
+    println(sbu)
   }
 }
